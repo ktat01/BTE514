631a3cf93c4144f51bf199a02b3f9ba727f1bdb5
Your Name <you@example.com>
2018-10-04 15:39:14 +0300

    src

---
+++ src/ders1.java
@@ -0,0 +1,44 @@
+
+public class ders1 {
+
+
+    public static void main(String args[]) {
+
+        /*
+        int x;
+
+        x=9;
+        x=x+10;
+        x=x+x+30;
+
+        System.out.println(x+x);
+
+
+        String y = "kubra";
+        String z = "gundur diyette";
+        byte d;
+        d = 17;
+
+        System.out.println(y + " " + d + " " + z);
+
+        d = (byte) (d+9);
+
+        System.out.println(y + " " + d + " " + z);
+        */
+
+
+
+        double myPi =3.1415;
+        String a = "7";
+        boolean truefalse = false;
+
+        Double.toString(myPi);
+        int c = Integer.parseInt(a);
+        Boolean.toString(truefalse);
+
+        c*= 3;
+
+        System.out.println(c);
+
+    }
+}
